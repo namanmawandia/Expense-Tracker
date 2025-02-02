da2507b8df5e49cf6fa3a982c9a1216bcfe82b7d
Naman Mawandia <namanmawandia@gmail.com>
2025-02-02 18:00:52 -0500

    activity_add added

---
+++ app/src/main/java/com/example/expensetracker/ActivityAdd.kt
@@ -0,0 +1,11 @@
+package com.example.expensetracker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class ActivityAdd : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
