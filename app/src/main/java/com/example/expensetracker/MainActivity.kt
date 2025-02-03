93016d7f7aa245944d920feb2626a84a68ba5ca4
Naman Mawandia <namanmawandia@gmail.com>
2025-02-02 19:17:41 -0500

    linked add button and back button

--- app/src/main/java/com/example/expensetracker/MainActivity.kt
+++ app/src/main/java/com/example/expensetracker/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.expensetracker
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class MainActivity : AppCompatActivity() {
@@ -12,8 +16,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val toolbar: Toolbar = findViewById(R.id.toolbar)
+        val btnFab: FloatingActionButton = findViewById(R.id.btnFAB)
+
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        btnFab.setOnClickListener{
+            setAddButton(this)
+        }
+
+    }
+
+    private fun setAddButton(context: Context) {
+
+        val intent = Intent(context,ActivityAdd::class.java)
+        Log.d("Main Activity", "setAddButton: Intent to another Add Activty")
+        startActivity(intent)
 
     }
 }
