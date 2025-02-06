82563869267348dd7b5586b02eec3dec8469924a
Naman Mawandia <namanmawandia@gmail.com>
2025-02-05 22:35:08 -0500

    setting up main activtiy

--- app/src/main/java/com/example/expensetracker/MainActivity.kt
+++ app/src/main/java/com/example/expensetracker/MainActivity.kt
@@ -4,8 +4,12 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
@@ -16,12 +20,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btnFab: FloatingActionButton = findViewById(R.id.btnFAB)
+        val tvDaily = findViewById<TextView>(R.id.tvDaily)
+        val tvCalender = findViewById<TextView>(R.id.tvCalender)
+        val ivTransac = findViewById<ImageView>(R.id.ivTransac)
+        val ivStats = findViewById<ImageView>(R.id.ivStats)
 
+
+
+        if(savedInstanceState==null){
+            replaeFragment(DailyFragment())
+        }
 
         btnFab.setOnClickListener{
             setAddButton(this)
         }
 
+    }
+
+    private fun replaeFragment(dailyFragment: Fragment) {
+
+        val fragment = DailyFragment()
+        val trasaction = supportFragmentManager.beginTransaction()
+        trasaction.replace(R.id.frgOuter,fragment)
+        trasaction.commit()
     }
 
     private fun setAddButton(context: Context) {
