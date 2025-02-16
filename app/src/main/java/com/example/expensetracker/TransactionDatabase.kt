fd488c84d6e896a67c9f8ac31fb879da41c8ba2b
Naman Mawandia <namanmawandia@gmail.com>
2025-02-16 14:09:47 -0500

    started with room database

---
+++ app/src/main/java/com/example/expensetracker/TransactionDatabase.kt
@@ -0,0 +1,29 @@
+package com.example.expensetracker
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Transaction::class], version = 1, exportSchema = false)
+abstract class TransactionDatabase() : RoomDatabase(){
+
+    abstract fun transactinoDao() : TransactionDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: TransactionDatabase? = null
+
+        fun getDatabase(context: Context): TransactionDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    TransactionDatabase::class.java,
+                    "transaction_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
