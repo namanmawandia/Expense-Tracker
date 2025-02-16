efac3ceee620cc9eac5b4f6052cdabb2c773ce82
Naman Mawandia <namanmawandia@gmail.com>
2025-02-15 22:18:43 -0500

    popupgrid complete

--- app/src/main/java/com/example/expensetracker/ActivityAdd.kt
+++ app/src/main/java/com/example/expensetracker/ActivityAdd.kt
@@ -1,8 +1,13 @@
 package com.example.expensetracker
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.WindowManager
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.GridView
+import android.widget.PopupWindow
 import android.widget.TextView
 import android.widget.Toolbar
 import androidx.activity.ComponentActivity
@@ -31,6 +36,23 @@ class ActivityAdd : ComponentActivity() {
     }
 
     private fun showPopGridView(tvCategoryValue: TextView) {
+
+        val inflater = LayoutInflater.from(this)
+        val layout = inflater.inflate(R.layout.popup_grid, null)
+
+        val gridView : GridView = findViewById(R.id.gridItem)
+        val categories = arrayOf("🍔 Food", "🚕 Transport", "💄 Beauty", "🎁 Gift", "🏠 Household", "🎓 Education")
+
+        val adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,categories)
+        gridView.adapter = adapter
+
+        val popupWindow = PopupWindow(layout,WindowManager.LayoutParams.MATCH_PARENT,WindowManager.LayoutParams.WRAP_CONTENT,true)
+        popupWindow.showAtLocation(tvCategoryValue,android.view.Gravity.BOTTOM,0,0)
+
+        gridView.setOnItemClickListener { _, _, position, _ ->
+            tvCategoryValue.text = categories[position]
+            popupWindow.dismiss()
+        }
 
     }
 }
