d5fa8795a71ec369992470434b732f11e4a554d7
Naman Mawandia <namanmawandia@gmail.com>
2025-05-26 17:29:25 -0400

    Added spinner and bar_layout.xml

--- app/src/main/java/com/example/expensetracker/MainActivity.kt
+++ app/src/main/java/com/example/expensetracker/MainActivity.kt
@@ -3,16 +3,25 @@ package com.example.expensetracker
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
 import kotlin.Pair
 import android.widget.ImageView
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.util.Calendar
 
 
 class MainActivity : AppCompatActivity() {
+
+    val months = arrayOf("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct",
+        "Nov", "Dec")
+    val years = (2010..2040).toList()
+    var globalMonth: Int = 0
+    var globalYear : Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +33,13 @@ class MainActivity : AppCompatActivity() {
         val tvCalender = findViewById<TextView>(R.id.tvCalender)
         val ivTransac = findViewById<ImageView>(R.id.ivTransac)
         val ivStats = findViewById<ImageView>(R.id.ivStats)
+        val monthSpinner = findViewById<Spinner>(R.id.monthSpinner)
+        val yearSpinner = findViewById<Spinner>(R.id.yearSpinner)
+        val ivLeftArrow = findViewById<ImageView>(R.id.ivLeftArrow)
+        val ivRightArrow = findViewById<ImageView>(R.id.ivRightArrow)
+
+        // setting up spinner
+        setupSpinner(monthSpinner,yearSpinner)
 
         if(savedInstanceState==null){
             replaceFragment(DailyFragment())
@@ -52,7 +68,43 @@ class MainActivity : AppCompatActivity() {
             Log.d("Main Activity", "setAddButton: Intent to Add Activity")
             startActivity(intent)
         }
+//        monthSpinner.setOnClickListener{
+//            TODO("left")
+//        }
+//        yearSpinner.setOnClickListener{
+//            TODO("Left")
+//        }
+        ivLeftArrow.setOnClickListener{
+            if(globalMonth==0) {
+                globalMonth = 11
+                globalYear--
+            } else globalMonth--
+            monthSpinner.setSelection(globalMonth)
+            yearSpinner.setSelection(globalYear)
+        }
+        ivRightArrow.setOnClickListener{
+            if(globalMonth==11) {
+                globalMonth = 0
+                globalYear++
+            } else globalMonth++
+            monthSpinner.setSelection(globalMonth)
+            yearSpinner.setSelection(globalYear)
+        }
 
+    }
+
+    private fun setupSpinner(monthSpinner: Spinner, yearSpinner: Spinner) {
+        val monthAdapter = ArrayAdapter(this, R.layout.spinner_item_bar_layout, months)
+        val yearAdapter = ArrayAdapter(this, R.layout.spinner_item_bar_layout, years)
+        val cal = Calendar.getInstance()
+        globalMonth = cal.get(Calendar.MONTH)
+        globalYear  = cal.get(Calendar.YEAR) -2010
+        monthAdapter.setDropDownViewResource(R.layout.spinner_item_bar_layout)
+        yearAdapter.setDropDownViewResource(R.layout.spinner_item_bar_layout)
+        monthSpinner.adapter = monthAdapter
+        yearSpinner.adapter = yearAdapter
+        monthSpinner.setSelection(globalMonth)
+        yearSpinner.setSelection(globalYear)
     }
 
     private fun setTargetActivity(targetActivity: Class<*>) {
