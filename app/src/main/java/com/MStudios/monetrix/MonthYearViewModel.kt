978d50178f360e3df626b75fe984849e63a61a9d
Naman Mawandia <namanmawandia@gmail.com>
2026-02-10 20:24:29 -0500

    added the list declaration of month and year to MonthYearViewModel

--- app/src/main/java/com/MStudios/monetrix/MonthYearViewModel.kt
+++ app/src/main/java/com/MStudios/monetrix/MonthYearViewModel.kt
@@ -3,6 +3,18 @@ package com.MStudios.monetrix
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
+val months = listOf(
+    "January", "February", "March", "April",
+    "May", "June", "July", "August",
+    "September", "October", "November", "December"
+    )
+
+fun yearsInRange(start: Int, end: Int): List<String> {
+    return (start..end).map { it.toString() }
+}
+
+val years = yearsInRange(2000, 2045)
+
 class MonthYearViewModel : ViewModel(){
     val selectedMonth = MutableLiveData<Int>()
     val selectedYear = MutableLiveData<Int>()
